b2fce37a14e38d2d78c26aa348316ba06d734584
Natalie Niemeir <nniemeir@protonmail.com>
2022-01-09 16:43:52 -0600

    improved variable names

--- filogbook.py
+++ filogbook.py
@@ -6,31 +6,31 @@ import os
 from os.path import exists
 from datetime import datetime
 while True:
-  LOG_CHOICE = input("1. New Logbook\n2. Add/Subtract From Existing Logbook\n3. Delete Existing Logbook\n4. View Existing Logbook\n")
+  log_choice = input("1. New Logbook\n2. Add/Subtract From Existing Logbook\n3. Delete Existing Logbook\n4. View Existing Logbook\n")
   file_exists = exists("CB.txt")
-  if LOG_CHOICE == "1":
+  if log_choice == "1":
     file_exists = exists("CB.txt")
     if file_exists == True:
         print("Logbook already exists")
     else:
       #CB.txt is opened in write mode and user input is written to it
       #Same input is written in a more readable format to the log
-      LOG_FILE = open("CB.txt", "x")
-      INITIAL_BALANCE = input("Enter Balance:\n")
-      LOG_FILE.write(INITIAL_BALANCE)
-      FIRSTENTRY = open("LOG.txt", "a")
-      FIRSTENTRY.write(f"\n Initial Balance Set to ${INITIAL_BALANCE}")
-      FIRSTENTRY.close()
+      log_file = open("CB.txt", "x")
+      initial_balance = input("Enter Balance:\n")
+      log_file.write(initial_balance)
+      first_entry = open("LOG.txt", "a")
+      first_entry.write(f"\n Initial Balance Set to ${initial_balance}")
+      first_entry.close()
       print("Logbook Created")
     quit()
-  elif LOG_CHOICE == "2":
+  elif log_choice == "2":
     file_exists = exists("CB.txt")
     if file_exists == True:
         print("")
     else:
         print("Logbook Does Not Exist")
         quit()
-  elif LOG_CHOICE == "3":
+  elif log_choice == "3":
     #If balance file exists, it is deleted along with the log file
     if file_exists == True:
       os.remove("CB.txt")
@@ -40,7 +40,7 @@ while True:
     else:
       print("Logbook Does Not Exist")
       quit()
-  elif LOG_CHOICE == "4":
+  elif log_choice == "4":
     if file_exists == True:
       logdisplay = open("LOG.txt", "r")
       s = logdisplay.read()
@@ -55,39 +55,39 @@ while True:
     quit()
   #CB.txt is read into variable c
   with open ("CB.txt", "r") as myfile:
-    c=myfile.readlines()
+    current_balance_file=myfile.readlines()
     line_number = 1
-    a = 0
-    BALANCE = ''
+    empty_file = 0
+    balance = ''
     #If CB.txt is empty, nothing happens
-    for b in c:
-      if a > line_number:
+    for new_lines in current_balance_file:
+      if empty_file > line_number:
         break
       else:
-        #The first line is written into the BALANCE variable
-        if a == line_number-1 and b != '\n':
-          BALANCE += b
-        elif b == '\n':
-          a += 1
-  print(f"Current Balance is ${BALANCE}")
-  BALANCE_CHANGE = input("Enter amount to change balance by\n")
+        #The first line is written into the balance variable
+        if empty_file == line_number-1 and new_lines != '\n':
+          balance += new_lines
+        elif new_lines == '\n':
+          empty_file += 1
+  print(f"Current Balance is ${balance}")
+  balance_change = input("Enter amount to change balance by\n")
   #Variables are converted to floats for calculation
-  NEW_BALANCE = float(BALANCE) + float(BALANCE_CHANGE)
-  DESCRIPTION = input("Reason for change?\n")
-  print(f"Your new balance is ${NEW_BALANCE}")
+  new_balance = float(balance) + float(balance_change)
+  description = input("Reason for change?\n")
+  print(f"Your new balance is ${new_balance}")
   #New balance replaces the old one in the balance file
-  BC = open("CB.txt", "rt")
-  change = BC.read()
-  change = change.replace(str(BALANCE), str(NEW_BALANCE))
-  BC.close()
-  BC = open("CB.txt", "wt")
-  BC.write(change)
-  BC.close()
-  #Date and time written to REGIONAL_TIME using Datetime module
+  balance_replacement = open("CB.txt", "rt")
+  change = balance_replacement.read()
+  change = change.replace(str(balance), str(new_balance))
+  balance_replacement.close()
+  balance_replacement = open("CB.txt", "wt")
+  balance_replacement.write(change)
+  balance_replacement.close()
+  #Date and time written to regional_time using Datetime module
   now = datetime.now()
-  REGIONAL_TIME = now.strftime("%m/%d/%Y %H:%M:%S")
+  regional_time = now.strftime("%m/%d/%Y %H:%M:%S")
   #New line containing details of balance change added to log file
   log = open("LOG.txt", "a")
-  log.write(f"\n {REGIONAL_TIME} | Balance changed by ${BALANCE_CHANGE} | Description: {DESCRIPTION} | New Balance: ${NEW_BALANCE}")
+  log.write(f"\n {regional_time} | Balance changed by ${balance_change} | Description: {description} | New Balance: ${new_balance}")
   log.close()
   quit()
